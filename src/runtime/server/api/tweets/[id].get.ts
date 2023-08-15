c87ac1ca6b822640f43803504993440937d39b3e
timb-103 <tim103@pm.me>
2023-08-15 14:57:51 +1000

    Trying different url structure

--- src/runtime/server/api/tweets/[id].get.ts
+++ src/runtime/server/api/tweets/[id].get.ts
@@ -12,35 +12,35 @@ export default cachedEventHandler(
     }
 
     try {
-      // @ts-ignore
-      const response: Tweet = await $fetch(
-        "https://cdn.syndication.twimg.com/tweet-result",
-        {
-          method: "GET",
-          responseType: "json",
-          headers: {
-            "User-Agent": "tc-bot/1.0",
-          },
-          params: {
-            id,
-            lang: "en",
-            token: getToken(id),
-            features: [
-              "tfw_timeline_list:",
-              "tfw_follower_count_sunset:true",
-              "tfw_tweet_edit_backend:on",
-              "tfw_refsrc_session:on",
-              "tfw_show_business_verified_badge:on",
-              "tfw_duplicate_scribes_to_settings:on",
-              "tfw_show_blue_verified_badge:on",
-              "tfw_legacy_timeline_sunset:true",
-              "tfw_show_gov_verified_badge:on",
-              "tfw_show_business_affiliate_badge:on",
-              "tfw_tweet_edit_frontend:on",
-            ].join(";"),
-          },
-        }
+      const url = new URL(`https://cdn.syndication.twimg.com/tweet-result`);
+
+      url.searchParams.set("id", id);
+      url.searchParams.set("lang", "en");
+      url.searchParams.set(
+        "features",
+        [
+          "tfw_timeline_list:",
+          "tfw_follower_count_sunset:true",
+          "tfw_tweet_edit_backend:on",
+          "tfw_refsrc_session:on",
+          "tfw_fosnr_soft_interventions_enabled:on",
+          "tfw_show_birdwatch_pivots_enabled:on",
+          "tfw_show_business_verified_badge:on",
+          "tfw_duplicate_scribes_to_settings:on",
+          "tfw_use_profile_image_shape_enabled:on",
+          "tfw_show_blue_verified_badge:on",
+          "tfw_legacy_timeline_sunset:true",
+          "tfw_show_gov_verified_badge:on",
+          "tfw_show_business_affiliate_badge:on",
+          "tfw_tweet_edit_frontend:on",
+        ].join(";")
       );
+      url.searchParams.set("token", getToken(id));
+
+      console.log(url.toString());
+
+      // @ts-ignore
+      const response: Tweet = await $fetch(url.toString());
 
       if (response) {
         return {
